3c306ef019cf66c01ceac2046a303ca3ba5d426e
oof2020 <71726040+oof2020@users.noreply.github.com>
2020-09-24 13:00:41 -0400

    Update average.py

--- average.py
+++ average.py
@@ -1,5 +1,5 @@
 int(input("please enter the number of tests you want to average: "))
-score1 = int(input("please enter a score: ")
-score2 = int(input("please enter a score: ")
+score1 = int(input("please enter a score: "))
+score2 = int(input("please enter a score: "))
 average = (score1 + score2)/2
 print("The average is ",average)
