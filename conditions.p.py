fb2d32a33b7ef22d64d06e23e5413f7e1d018026
oof2020 <71726040+oof2020@users.noreply.github.com>
2020-10-08 02:33:53 -0400

    conditions assignment

---
+++ conditions.p.py
@@ -0,0 +1,14 @@
+temp = 0
+while not temp < 999:
+    temp = int(input("please enter the current temperature"))
+    if temp > 90:
+        print("wear shorts")
+    elif temp > 70:
+        print("short sleeves are fine")
+    elif temp > 50:
+        print("wear a jacket")
+    elif temp > 32:
+        print("wear a heavy coat")
+    else:
+        print("stay inside")
+        
