6a957a41df152e33f5b4e8021869b6c4762f91b3
oof2020 <71726040+oof2020@users.noreply.github.com>
2020-11-24 12:46:18 -0500

    Python Queue simulation
    could not get it to quite work not sure what the error is

---
+++ python queue 1.py
@@ -0,0 +1,12 @@
+names = ["Max", "Charles", "Billy", "Bob", "Daniel", "Mike", "Lucas", "Scott", "Stiles", "Tony"]
+numberofnames = 0
+
+for numberofnames in range (0,10):
+    acceptednames = int(input(Please enter a name: ")
+    names.append(acceptednames)
+    numberofnames = numberofnames  + 1
+
+for numberofnames in range (0,10 len(names)):
+    print(names.pop(0))
+        
+    
